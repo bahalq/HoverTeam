ccc77a15406019719ba55ee56edd8e54e71da91a
bahalq <bahalqadam@gmail.com>
2025-11-23 13:23:07 +0100

    Delete select.php

--- select.php
+++
@@ -1,6 +0,0 @@
-<?php
-    // $select = $db->prepare('SELECT * FROM info ORDER BY id DESC LIMIT 1');
-    $select = $db->prepare('SELECT * FROM info ');
-    $select->execute();
-    $_SESSION['info'] = $select->fetchAll(PDO::FETCH_ASSOC);
-?>
